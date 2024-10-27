7643f8db49762d1eb6c5488e71dd297978016e37
FaizanTechy <faizanraza112112@gmail.com>
2024-10-27 10:01:24 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+var para=document.querySelector('Para')
+
+var btn=document.querySelector('.btn').addEventListener('click',function(){
+
+    document.querySelector('.Para').style.color='red';
+    document.querySelector('.Para').style.backgroundColor='black';
+
+
+
+})
+
